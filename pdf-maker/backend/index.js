6c18a9e5584f4180ad09ac42a0ab47d54b94c1e5
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-29 22:25:58 +0530

    feat(pdf): implement PDF generation using Puppeteer

---
+++ pdf-maker/backend/index.js
@@ -0,0 +1,30 @@
+import puppeteer from "puppeteer";
+import path from "path";
+
+async function generatePDF() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // project root directory
+  const rootDir = process.cwd();
+
+  // relative path → absolute path
+  const htmlPath = path.join(rootDir, "template", "index.html");
+
+  // load local HTML file
+  await page.goto(`file://${htmlPath}`, {
+    waitUntil: "networkidle0",
+  });
+
+  // generate PDF
+  await page.pdf({
+    path: "generate.pdf",
+    format: "A4",
+    printBackground: true,
+  });
+
+  await browser.close();
+  console.log("PDF generated successfully");
+}
+
+generatePDF();
